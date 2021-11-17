5eb16ff6ba5231bbb769b3d5dce4e1ee52e695a1
Diego Rodrigo Fernandez Zamora <deltphoenixsis@outlook.com>
2021-11-16 19:47:56 -0600

    carga trabajo final

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { HomeComponent } from './home/home.component';
+
+import {AmplifyAngularModule, AmplifyService} from 'aws-amplify-angular'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AmplifyAngularModule
+  ],
+  providers: [AmplifyService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
